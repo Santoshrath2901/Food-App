2755ae037aebb7e61b4d90ddade3243d83495956
Santosh Rath <santosh.rath29@gmail.com>
2023-11-08 20:09:13 +0530

    Using React hooks(useState)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,33 @@
-import resList from "../utils/mockData"
-import RestaurantCard from "./RestaurantCard"
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+import resList from "../utils/mockData";
 
-function cheap(){
-  if(restaurant.data.costForTwo<300)
-  
-}
+const Body = () => {
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
 
-const Body = () => (
+  return (
     <div className="body">
-    <div className="cheap">
-      <button onclick={cheap}>Less than 3000 </button>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestraunt(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {listOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
     </div>
-    <div className="res-container">
-      {resList.map((restaurant) => (
-        <RestaurantCard key={restaurant.data.id} resData={restaurant} />
-      ))}
-    </div>
-  </div>
-)
+  );
+};
 
-export default Body
+export default Body;
