be6cfc2b10ceb5107a3470a3a9cb57393ef5b2d8
Santosh Rath <santosh.rath29@gmail.com>
2024-01-05 11:37:15 +0530

    use of redux-toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
-
+import { useSelector } from "react-redux";
 
 
 const Header = () => {
@@ -11,7 +11,11 @@ const Header = () => {
   const onlineStatus = useOnlineStatus();
 
   const { loggedInUser } = useContext(UserContext);
-  console.log(loggedInUser);
+ //console.log(loggedInUser);
+
+  // Subscribing to the store using a Selector
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
 
 
   return (
@@ -34,7 +38,9 @@ const Header = () => {
           <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li>Cart</li>
+          <li className="px-4 font-bold text-xl">
+            <Link to="/cart">Cart - ({cartItems.length} items)</Link>
+          </li>
           <button
             className="login"
             onClick={() => {
@@ -46,7 +52,7 @@ const Header = () => {
             {btnNameReact}
           </button>
 
-          <li className="px-4 font-bold">{loggedInUser}</li>
+          <li className="px-4 ">{loggedInUser}</li>
         </ul>
       </div>
     </div>
